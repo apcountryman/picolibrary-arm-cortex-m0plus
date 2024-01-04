283555347f31f1b775cd16d0213fce827bf32ef2
Andrew Countryman <apcountryman@gmail.com>
2024-01-04 12:12:43 -0800

    Add Arm Cortex-M0+ interrupt vector table (#41)
    Resolves #36 (Add Arm Cortex-M0+ interrupt vector table).

---
+++ configuration/release/include/picolibrary/arm/cortex/m0plus/implementation/interrupt/vectors.h
@@ -0,0 +1,85 @@
+/**
+ * picolibrary-arm-cortex-m0plus
+ *
+ * Copyright 2023, Andrew Countryman <apcountryman@gmail.com> and the
+ * picolibrary-arm-cortex-m0plus contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Arm::Cortex::M0PLUS implementation interrupt vectors.
+ */
+
+Handler interrupt_0_handler;
+
+Handler interrupt_1_handler;
+
+Handler interrupt_2_handler;
+
+Handler interrupt_3_handler;
+
+Handler interrupt_4_handler;
+
+Handler interrupt_5_handler;
+
+Handler interrupt_6_handler;
+
+Handler interrupt_7_handler;
+
+Handler interrupt_8_handler;
+
+Handler interrupt_9_handler;
+
+Handler interrupt_10_handler;
+
+Handler interrupt_11_handler;
+
+Handler interrupt_12_handler;
+
+Handler interrupt_13_handler;
+
+Handler interrupt_14_handler;
+
+Handler interrupt_15_handler;
+
+Handler interrupt_16_handler;
+
+Handler interrupt_17_handler;
+
+Handler interrupt_18_handler;
+
+Handler interrupt_19_handler;
+
+Handler interrupt_20_handler;
+
+Handler interrupt_21_handler;
+
+Handler interrupt_22_handler;
+
+Handler interrupt_23_handler;
+
+Handler interrupt_24_handler;
+
+Handler interrupt_25_handler;
+
+Handler interrupt_26_handler;
+
+Handler interrupt_27_handler;
+
+Handler interrupt_28_handler;
+
+Handler interrupt_29_handler;
+
+Handler interrupt_30_handler;
+
+Handler interrupt_31_handler;
--- docs/library_configuration.md
+++ docs/library_configuration.md
@@ -3,6 +3,7 @@
 ## Table of Contents
 1. [Overview](#overview)
 1. [General Configuration](#general-configuration)
+1. [Interrupt Vector Table Configuration](#interrupt-vector-table-configuration)
 
 ## Overview
 picolibrary-arm-cortex-m0plus is configured for use with a specific Arm Cortex-M0+
@@ -42,4 +43,74 @@ for the Microchip SAM D21/DA1 family of Arm Cortex-M0+ microcontrollers:
 #define PICOLIBRARY_ARM_CORTEX_M0PLUS_IMPLEMENTATION_INTERRUPTS 28
 
 #endif // PICOLIBRARY_ARM_CORTEX_M0PLUS_IMPLEMENTATION_CONFIGURATION_H
+```
+
+## Interrupt Vector Table Configuration
+Interrupt vector table configuration is performed by an implementation's
+`picolibrary/arm/cortex/m0plus/implementation/interrupt/vectors.h` header file.
+This header file is used to inject implementation interrupt handler entries into
+`::picolibrary::Arm::Cortex::M0PLUS::Interrupt::Vector_Table`.
+
+`picolibrary/arm/cortex/m0plus/implementation/interrupt/vectors.h` would be defined as
+follows for the Microchip SAM D21/DA1 family of Arm Cortex-M0+ microcontrollers:
+```c++
+Handler pm_handler;
+
+Handler sysctrl_handler;
+
+Handler wdt_handler;
+
+Handler rtc_handler;
+
+Handler eic_handler;
+
+Handler nvmctrl_handler;
+
+Handler dmac_handler;
+
+Handler usb_handler;
+
+Handler evsys_handler;
+
+Handler sercom0_handler;
+
+Handler sercom1_handler;
+
+Handler sercom2_handler;
+
+Handler sercom3_handler;
+
+Handler sercom4_handler;
+
+Handler sercom5_handler;
+
+Handler tcc0_handler;
+
+Handler tcc1_handler;
+
+Handler tcc2_handler;
+
+Handler tc3_handler;
+
+Handler tc4_handler;
+
+Handler tc5_handler;
+
+Handler tc6_handler;
+
+Handler tc7_handler;
+
+Handler adc_handler;
+
+Handler ac_handler;
+
+Handler dac_handler;
+
+Handler ptc_handler;
+
+Handler i2s_handler;
+
+Handler ac1_handler;
+
+Handler tcc3_handler;
 ```
